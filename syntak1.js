b6cad2685d8565c0ef3953ebbbb0d3efdcfeae63
Hany zahra nabbilla <113100305+Hanyzahra@users.noreply.github.com>
2022-09-14 13:49:30 +0700

    Create syntak1.js

---
+++ syntak1.js
@@ -0,0 +1,29 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName;
+firstName = "Hany Zahra";
+let lastName;
+lastName = "Nabbilla";
+let age;
+age = 21;
+let isMarried = false ;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
+
